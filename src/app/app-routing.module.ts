eb6317791281c4db47efb3f9b6c84d5cbdd32db4
Roni <roni12@gmail.com>
2020-02-11 19:11:38 +0200

    lint changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PlayerComponent } from './player/player.component';
-import { TranscriptComponent } from './transcript/transcript.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PlayerComponent } from './components/player/player.component';
+import { TranscriptComponent } from './components/transcript/transcript.component';
 
 @NgModule({
   declarations: [
